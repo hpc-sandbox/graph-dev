7916e27f35c8d383773a76e92e9560883c75a8fe
ghos167 <sayan.ghosh@pnnl.gov>
2021-08-18 18:12:39 -0700

    updated

--- call-atomic-intrinsic-in-kernel-region/test.cpp
+++ call-atomic-intrinsic-in-kernel-region/test.cpp
@@ -27,83 +27,41 @@ class Dummy
     {
       delete[] array_;
     }
-    
-  T* array_;
-  int n_; 
-};
-	
-void nbrscan_atomic(Dummy<int>* d)
-{
-  const int n_ = d->n_;
-  int* atarr;
-  int compare = 5;
-  int val = 6;
-  bool week = false;
-  int success_order = 1;
-  int failure_order = 0;
+  
+   /* 
+   Dummy(const Dummy &d) 
+   {
+     n_ = d.n_;
+     memcpy(array_, d.array_, sizeof(T)*n_); 
+   } 
+   */
 
-  atarr = (int*)malloc(n_*sizeof(int));
-  memset(&atarr, 0, n_*sizeof(int));
+   Dummy(const Dummy &d) = delete;
+   Dummy& operator=(const Dummy& d) = delete;
 
-#pragma omp target teams distribute parallel for \
-       map(tofrom:atarr[0:n_])
-  for (int i = 0; i < n_; i++)
-  {
-      atarr[i] += 1;
-     if(__atomic_compare_exchange(&atarr[i], &compare, &val, week,success_order, failure_order)){
-     // std::cout << "---Fail----" << atarr[n_-1] << std::endl;
+   void atomic_test()
+   {
+     int compare = 5;
+     int val = 6;
+     bool week = false;
+     int success_order = 1;
+     int failure_order = 0;
+
+#pragma omp target teams distribute parallel for 
+     for (int i = 0; i < n_; i++)
+     {
+       __atomic_compare_exchange(&array_[i], &compare, &val, week, success_order, failure_order);
      }
-  }
-    
-  std::cout << "---Success----" << atarr[n_-1] << std::endl;
-  free(atarr);
-}
 
-/**
-void nbrscan_atomic2(Dummy<int>* d)
-{
-  const long n_ = d->n_;
-  int*atarr, *atarr_dev;
-  int compare = 5;
-  int val = 6;
-  bool week = false;
-  int success_order = 1;
-  int failure_order = 0;
+     std::cout << "---Success----" << array_[n_-1] << std::endl;
+   }
 
-  
-  int h = omp_get_initial_device();
-  int t = omp_get_default_device();
+  //private:   
+   T* array_;
+   int n_; 
+};
 
-  if (omp_get_num_devices() < 1 || t < 0)
-  {
-    std::cout << " ERROR: No device found.\n";
-    std::abort();
-  }
-
-  atarr = (int*)malloc(n_*sizeof(int));
-  memset(atarr, 0, sizeof(int)*n_);
-  
-  atarr_dev = (int*)omp_target_alloc(sizeof(int)*n_, t);
-  omp_target_memcpy(atarr_dev, atarr, sizeof(int)*n_, 0, 0, t, h);
-
-#pragma omp target teams distribute parallel for \
-  is_device_ptr(atarr_dev)
-  for (int i = 0; i < n_; i++)
-  {
-      atarr[i] += 1;
-      //int res = atomicCAS(atarr[i],compare,val);
-      //int res =  fetch_and_add<int>(&atarr_dev[i], 1);
-      if(__atomic_compare_exchange(&atarr[i], &compare, &val, week,success_order, failure_order)){
-      // std::cout << "---Fail----" << atarr[n_-1] << std::endl;
-      } 
-  }
-
-  omp_target_memcpy(atarr, atarr_dev, sizeof(int)*n_, 0, 0, h, t);
-  omp_target_free(atarr_dev, t);
-  std::cout <<"---Success----" << atarr[n_-1] << std::endl;
-  free(atarr);
-}
-*/
+template class Dummy<int>;
 
 int main(int argc, char *argv[])
 {
@@ -123,11 +81,13 @@ int main(int argc, char *argv[])
   
   std::cout << "Size: " << n << std::endl;
   Dummy<int> *d = new Dummy<int>(n);
+  const int x = d->n_;
 
-  std::cout << "Access mapped array.\n";
-  nbrscan_atomic(d);
-  std::cout << "Access device pointer.\n";
- // nbrscan_atomic2(d);
+#pragma omp target enter data map(to:d) map(to:d->array_[0:x])
+  d->atomic_test();
+#pragma omp target exit data map(from:d->array_[0:x])
+ 
+  delete d;
 
   return 0;
 }
